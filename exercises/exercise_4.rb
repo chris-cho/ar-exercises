31372e7f87fc230d4b870f985f2a7c7c9c37a746
chris-cho <hyunsangc@gmail.com>
2022-01-08 23:10:43 -0500

    add ex4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,37 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Surrey = Store.new do |s|
+  s.name = "Surrey"
+  s.annual_revenue = "224000"
+  s.mens_apparel = false
+  s.womens_apparel = true
+end
+
+Whistler = Store.new do |s|
+  s.name = "Whistler"
+  s.annual_revenue = "1900000"
+  s.mens_apparel = true
+  s.womens_apparel = false
+end
+
+Yaletown = Store.new do |s|
+  s.name = "Yaletown"
+  s.annual_revenue = "430000"
+  s.mens_apparel = true
+  s.womens_apparel = true
+end
+
+Surrey.save
+Whistler.save
+Yaletown.save
+
+@mens_store = Store.where(mens_apparel: true)
+
+@mens_store.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_store = Store.where(womens_apparel: true).where(annual_revenue: 0..1000000)
