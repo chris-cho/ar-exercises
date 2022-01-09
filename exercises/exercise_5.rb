4f4928f2cbc9576480a5fb16256610b47e684674
chris-cho <hyunsangc@gmail.com>
2022-01-08 23:26:23 -0500

    add ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@stores_sum = Store.sum(:annual_revenue)
+
+@stores_avg = Store.average(:annual_revenue)
+
+@stores_gt = Store.where(Store.arel_table[:annual_revenue].gt(1000000)).size
+
+puts @stores_gt
