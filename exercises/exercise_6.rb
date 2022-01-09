d2689042aaa7638bb82c4dcac93089fede562ecd
chris-cho <hyunsangc@gmail.com>
2022-01-08 23:29:27 -0500

    add employees with ex6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Ariel", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Chris", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Ash", last_name: "Virani", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Kim", last_name: "Virani", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Brandon", last_name: "Virani", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Lucas", last_name: "Virani", hourly_rate: 60)
